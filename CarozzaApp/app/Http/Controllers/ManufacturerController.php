1573dc6c8d52558865cbe8a9af9c085c6e5bdd9a
dejanMT <dejan2mt@gmail.com>
2024-01-09 17:30:04 +0000

    completed task 3

--- CarozzaApp/app/Http/Controllers/ManufacturerController.php
+++ CarozzaApp/app/Http/Controllers/ManufacturerController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Manufacturer;
 
 class ManufacturerController extends Controller
 {
-    //
+    public function index(){
+        $manufacturers = Manufacturer::orderBy('name')->get();
+        return view('manufacturers.index', compact('manufacturers'));
+    }
 }
